aaf31db2a7739f627279499584f5a8a1deb08298
Jake_Kim <wer1915@gmail.com>
2026-03-20 08:24:06 +0900

    feat: 배치 실행 다이얼로그 구현 (Phase 6-A)
    - BatchDialog: 파일 목록 추가/제거, Start/Cancel, 진행률 + 결과 로그
      - WM_BATCH_PROGRESS / WM_BATCH_JOB_DONE / WM_BATCH_COMPLETE 처리
      - 실행 중 Start/Add/Remove 비활성, Cancel 버튼으로 취소 요청
    - SAGEDashDoc: OnAutomationBatch 핸들러 추가 (현재 프로젝트 규칙 전달)
    - Resource.h: IDD_BATCH / IDC_* / IDS_BATCH_* / ID_AUTOMATION_BATCH 추가
    - SAGEDash.rc: 다이얼로그 정의, 배치 로그 문자열, Automation 메뉴 항목 추가

--- SAGEDash/Resource.h
+++ SAGEDash/Resource.h
@@ -200,6 +200,27 @@
 #define IDS_LOG_FTP_OK                  11101
 #define IDS_LOG_FTP_FAIL                11102
 
+// 배치 실행 다이얼로그 (11201~)
+#define IDD_BATCH                       312
+#define IDC_LIST_BATCH_FILES            1020
+#define IDC_BTN_ADD_FILES               1021
+#define IDC_BTN_REMOVE_FILE             1022
+#define IDC_BTN_CLEAR_FILES             1023
+#define IDC_PROGRESS_BATCH              1024
+#define IDC_LIST_BATCH_LOG              1025
+#define IDC_STATIC_BATCH_SUMMARY        1026
+#define IDC_BTN_START_BATCH             1027
+#define ID_AUTOMATION_BATCH             32781
+
+#define IDS_LOG_BATCH_START             11201
+#define IDS_LOG_BATCH_JOB_OK            11202
+#define IDS_LOG_BATCH_JOB_FAIL          11203
+#define IDS_LOG_BATCH_DONE              11204
+#define IDS_LOG_BATCH_CANCELLED         11205
+#define IDS_BATCH_FILE_FILTER           11206
+#define IDS_BATCH_SUMMARY_FMT           11207
+#define IDS_BATCH_SUMMARY_CANCELLED     11208
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
